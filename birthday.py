7851952c0c756a9fdb907533f6a0ec1351572ffd
Lucas Rocha dos Santos <94807957+LogLucasRocha@users.noreply.github.com>
2023-07-03 09:59:26 -0300

    Delete birthday.py

--- birthday.py
+++
@@ -1,4 +0,0 @@
-dia=input('Digite o dia em que voce nasceu:')
-mes=input('Digite o mes em que voce nasceu:')
-ano=input('Digite o ano em que voce nasceu:')
-print('Voce nasceu na data', dia, '/', mes, '/', ano)
