0e6296aa18c0d1a2765ba66a2477fac5336f0869
Lucas Rocha <lucasrocha77706@gmail.com>
2023-07-08 08:57:09 -0300

    Atualização de Exercícios

---
+++ python-exercices/ex006.py
@@ -0,0 +1,8 @@
+import math
+
+x = int(input())
+dobro = x*2
+triplo = x*3
+sqrt = math.sqrt(x)
+
+print("O dobro de {} é {}, seu triplo é {} e sua raíz quadrada é {}".format(x, dobro, triplo, sqrt))
---
+++ python-exercices/ex007.py
@@ -0,0 +1,5 @@
+nota1 = float(input())
+nota2 = float(input())
+media = (nota1 + nota2) / 2
+
+print("A nota média desse aluno é {}".format(media))
