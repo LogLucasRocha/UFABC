57e91ee04fd9d4fd8a6c3abfb4389201a3b28dbf
Lucas Rocha <lucasrocha77706@gmail.com>
2023-07-19 10:29:24 -0300

    atualização funções

--- BCC/python-exercices/pi.py
+++ BCC/python-exercices/pi.py
@@ -1,12 +1,11 @@
 import math
 
+
 # Aproximação de Schneider
 
 def pi1():
     pi = math.sqrt(7 + math.sqrt(6 + math.sqrt(5)))
     print(pi)
-
-
 
 
 # Aproximação de S. Irvine
@@ -15,6 +14,6 @@ def pi2():
     pi = math.sqrt(math.sqrt((3 ** 4) + ((19 ** 2) / 22)))
     print(pi)
 
-pi1()
 
+pi1()
 pi2()
