d9204aef89af3096f3e8917bb884c999005924d9
Lucas Rocha <lucasrocha77706@gmail.com>
2023-07-21 08:52:21 -0300

    gráfico com duas raízes reais

--- BCC/funções gráficas.py
+++ BCC/funções gráficas.py
@@ -4,10 +4,26 @@ import matplotlib.pyplot as plot
 
 # Estudo de funções gráficas e arrays
 
-x = np.arange(0, 2 * math.pi, 0.1)
-f = np.sin(x)
-plot.plot(x, f)
-plot.title("Seno de X")
+# #Primeiro Gráfico
+# x = np.arange(0, 2 * math.pi, 0.1)
+# f = np.sin(x)
+# plot.plot(x, f)
+# plot.title("Seno de X")
+# plot.xlabel("X")
+# plot.ylabel("Y")
+# plot.grid(True, which="both")
+# plot.show()
+
+#Segundo Gráfico com raízes reais
+
+x = np.arange(1, 4.1, 0.1)
+y = x ** 2 - 5*x + 6
+plot.plot(x, y)
+plot.grid(True, which="both")
+plot.title("y = x^2 - 5x + 6")
 plot.xlabel("X")
 plot.ylabel("Y")
+plot.grid(True, which="both")
+plot.plot(2,0,'r*')
+plot.plot(3,0,'g*')
 plot.show()
