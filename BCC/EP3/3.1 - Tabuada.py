4cc40a2aa067c1d4cdd51203bc9ca74893646125
Lucas Rocha <lucasrocha77706@gmail.com>
2023-07-28 08:39:32 -0300

    3.1 - Tabuada - Concluido

---
+++ BCC/EP3/3.1 - Tabuada.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+
+for c in range(0, b+1):
+    r = a * c
+    print("{} x {} = {}".format(a, c, r))
---
+++ BCC/Estrutura de Repetição for/Exercicios - Curso em Vídeo/Ex 059.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+s = 0
+
+for g in range(a, f):
+    s += g
+
+print(s)
