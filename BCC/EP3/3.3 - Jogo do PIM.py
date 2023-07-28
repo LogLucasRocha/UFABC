4be151d1658e73cd268fe5b5b8a77f45687081b3
Lucas Rocha <lucasrocha77706@gmail.com>
2023-07-28 09:39:04 -0300

    Jogo do PIM - Concluido

--- BCC/EP3/3.3 - Jogo do PIM.py
+++ BCC/EP3/3.3 - Jogo do PIM.py
@@ -1,10 +1,17 @@
 N = int(input())
+
+
 def imprimir_com_pim(N):
-    for num in range(1, N + 1):
+    num = 1
+    count_pim = 0
+
+    while count_pim != N:
         if num % 4 == 0:
             print("PIM")
+            count_pim += 1
         else:
             print(num, end=" ")
+        num += 1
 
 
 imprimir_com_pim(N)
