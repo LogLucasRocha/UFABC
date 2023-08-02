78b028a6464afeaee48b9652b26c69541a9929b1
Lucas Rocha <lucasrocha77706@gmail.com>
2023-08-02 11:42:33 -0300

    Upgrade 3.4

--- BCC/EP3/3.4 - Contagem regressiva maluca.py
+++ BCC/EP3/3.4 - Contagem regressiva maluca.py
@@ -2,7 +2,7 @@ inicio = int(input())
 contador = 0
 
 while inicio >= contador:
-    inicio = inicio - contador
-    print('Faltam {} segundos'.format(inicio))
-    contador = contador + 2
+    inicio -= contador
+    print(f'Faltam {inicio} segundos')
+    contador += 2
 print('Acabou')
