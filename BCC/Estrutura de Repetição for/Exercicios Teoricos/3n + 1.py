d2e941bacd9f2a12fe88d58fb8bffe54ea2ed247
Lucas Rocha <lucasrocha77706@gmail.com>
2023-08-04 10:54:15 -0300

    Soma Numeros

--- BCC/Estrutura de Repetição for/Exercicios Teoricos/3n + 1.py
+++ BCC/Estrutura de Repetição for/Exercicios Teoricos/3n + 1.py
@@ -5,8 +5,10 @@ contador = 0
 while x > 1:
     if x % 2 == 0:
         x /= 2
+        print(x)
     elif x % 2 == 1:
         x = (x * 3) + 1
+        print(x)
     contador += 1
 
-print(contador+1)
+print(f'O número de passos até o fim do programa foi {contador + 1}')
---
+++ BCC/Estrutura de Repetição for/Exercicios Teoricos/Soma números.py
@@ -0,0 +1,8 @@
+natural = int(input())
+soma = 0
+
+while natural > 0:
+    soma += natural
+    natural = (int(input()))
+
+print(soma)
