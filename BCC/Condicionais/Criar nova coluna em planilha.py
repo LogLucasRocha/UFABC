a5e6954d480603ccb29ab7de34b0dc4f4b7e260b
Lucas Rocha <lucasrocha77706@gmail.com>
2023-08-08 09:04:37 -0300

    atualiza

--- BCC/Condicionais/Criar nova coluna em planilha.py
+++ BCC/Condicionais/Criar nova coluna em planilha.py
@@ -5,6 +5,10 @@ df = pd.read_csv('https://drive.google.com/u/1/uc?id=1ASOWhw5tp-kUbZcIlkg5eCKwZN
 opcao = input('Deseja calcular média? Aperse s para sim e n para não: ')
 
 if opcao == 's':
-    df['Média'] = (df['Prova 1'] + df['Prova 2'] + df['Trabalho'])/3
+    peso_1 = int(input('Digite o peso da prova 1: '))
+    peso_2 = int(input('Digite o peso da prova 2: '))
+    peso_trabalho = int(input('Digite o peso do trabalho: '))
+    soma_pesos = peso_1 + peso_2 + peso_trabalho
+    df['Média'] = ((df['Prova 1'] * peso_1) + (df['Prova 2'] * peso_2) + (df['Trabalho'] * peso_trabalho)) / soma_pesos
 
 print(df)
